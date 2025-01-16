dc313770e83ea56a4d0f6e8e2b18a4eb02dda1dc
Petr Balat <petr.balat@gmail.com>
2025-01-16 22:08:49 +0100

    OrderListener

--- src/main/kotlin/cz/softdeluxe/redpanda/testredpanda/listener/OrderListener.kt
+++ src/main/kotlin/cz/softdeluxe/redpanda/testredpanda/listener/OrderListener.kt
@@ -1,9 +1,15 @@
 package cz.softdeluxe.redpanda.testredpanda.listener
 
+import cz.softdeluxe.redpanda.testredpanda.dto.OrderDto
+import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
 
 @Component
 class OrderListener {
 
+    @KafkaListener(topics = ["orders"], groupId = "test-redpanda")
+    fun listen(orderDto: OrderDto): Unit {
+        println("Received order $orderDto")
+    }
 
 }
